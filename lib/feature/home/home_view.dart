fc72257df57f14a582eb2bd711bb670983c3aa6f
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-08-11 12:21:03 +0300

    delete listview.builder

--- lib/feature/home/home_view.dart
+++ lib/feature/home/home_view.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kartal/kartal.dart';
 
 import 'package:spacex_latest_launch/bloc/spacex_bloc.dart';
 import 'package:spacex_latest_launch/bloc/spacex_events.dart';
@@ -18,18 +19,18 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  Future<void> _refresh() async {
-    try {
-      await Future.delayed(const Duration(seconds: 2));
+  // Future<void> _refresh() async {
+  //   try {
+  //     await Future.delayed(const Duration(seconds: 2));
 
-      print('update data');
-      setState(() {
-        counter++;
-      });
-    } catch (error) {
-      debugPrint('${StringConstans.refleshError} $error');
-    }
-  }
+  //     print('update data');
+  //     setState(() {
+  //       counter++;
+  //     });
+  //   } catch (error) {
+  //     debugPrint('${StringConstans.refleshError} $error');
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +54,10 @@ class _HomeViewState extends State<HomeView> {
               return Text('Getting Error: ${state.message}');
             } else if (state is SpacexLoadedState) {
               return RefreshIndicator(
-                  onRefresh: _refresh,
+                  onRefresh: () async {
+                    BlocProvider.of<SpacexBloc>(context).add(LoadSpacexData());
+                  },
+                  //_refresh,
                   child: _LaunchDetailCard(
                     size: size,
                     state: state,
@@ -79,46 +83,44 @@ class _LaunchDetailCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(state.spacexModel.links.webcast);
-    print(state.spacexModel.links.reddit.launch);
-    print(state.spacexModel.details);
-    print(state.spacexModel.links.flickr.original.length);
-    return Card(
-      child: Padding(
+    // print(state.spacexModel.links.webcast);
+    // print(state.spacexModel.links.reddit.launch);
+    // print(state.spacexModel.details);
+    // print(state.spacexModel.links.flickr.original.length);
+    return Scaffold(
+      body: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: ListView.builder(
-          itemCount: state.spacexModel.crew.length,
-          itemBuilder: (context, index) {
-            final crewMember = state.spacexModel.crew[index];
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('${StringConstans.refleshed} $counter'),
-                Center(
-                  child: Image.network(state.spacexModel.links.patch.large,
-                      width: 170, height: 170),
-                ),
-                Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        state.spacexModel.name, // modelname,
-                      ),
-                      Text(state.spacexModel.dateLocal.toString()),
-                      Text(state.spacexModel.dateUtc.toString()),
-                      Text(state.spacexModel.links.webcast),
-                      Text(state.spacexModel.links.patch.small),
-                      Text(state.spacexModel.links.reddit.launch),
-                      //Text(state.spacexModel.links.reddit.media), // null
-                      //Text(state.spacexModel.launch.toString()), // null
-                      //Text(state.spacexModel.links.flickr.original[index])
-                      Text("${StringConstans.crewMember} :  $crewMember"),
-                    ],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('${StringConstans.refleshed} $counter'),
+            Center(
+              child: Image.network(
+                state.spacexModel.links.patch.small,
+              ),
+            ),
+            Center(
+              child: Column(
+                // final crewMember = state.spacexModel.crew[index];
+                children: [
+                  Text(
+                    state.spacexModel.name, // modelname,
+                    style: context.general.textTheme.displayMedium,
                   ),
-                ),
-              ],
-            );
-          },
+                  Text(state.spacexModel.dateLocal.toString()),
+                  Text(state.spacexModel.dateUtc.toString()),
+                  Text(state.spacexModel.links.webcast),
+                  Text(state.spacexModel.links.patch.small),
+                  Text(state.spacexModel.links.reddit.launch),
+
+                  //Text(state.spacexModel.links.reddit.media), // null
+                  //Text(state.spacexModel.launch.toString()), // null
+                  //Text(state.spacexModel.links.flickr.original[index])
+                  // Text("${StringConstans.crewMember} :  $crewMember"),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
